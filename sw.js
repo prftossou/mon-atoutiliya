572d77c6ddd2a0cfb8b44b45dac3cddb44a2cc1b
prftossou <tossparfait@gmail.com>
2025-09-10 18:45:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,20 @@
 self.addEventListener("install", (event) => {
+  console.log("Service Worker en cours d'installation...");
   event.waitUntil(
     caches.open("atoutiliya-cache").then((cache) => {
       return cache.addAll([
-        "/",
+        "/", // page d’accueil
         "/?utm_source=pwa",
-        "https://www.atoutiliya.com/"
+        "https://atoutiliya.com/" // ton site principal (⚠️ supprime le "www." pour éviter le mixed content)
       ]);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activé.");
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
